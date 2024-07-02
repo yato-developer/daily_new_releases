0b2394823f0561b1144496d1a5b61a1e920b1991
yato <yato28zzzz@gmail.com>
2024-07-02 15:50:27 +0900

    second comit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:daily_new_releases/firebase/firebase_options.dart';
 import 'package:daily_new_releases/screen/theme/theme.dart';
 import 'package:daily_new_releases/update/feature/top_page.dart';
 import 'package:daily_new_releases/update/feature/util/shared_preferences/shared_preferences_repository.dart';
-import 'package:dotenv/dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
